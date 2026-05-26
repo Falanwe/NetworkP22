109a586897c4cb38b5a6912505f260fa596f857e
Jean-Noël Gourdol <jngourdol@stormancer.com>
2026-05-26 15:38:00 +0200

    Tasks

---
+++ Asynchronism/TaskDemos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Asynchronism
+{
+    internal class TaskDemos
+    {
+        public static async Task<int> WorkForARandomTime()
+        {
+            var delay = Random.Shared.Next(2000);
+            await Task.Delay(delay);
+            return delay;
+        }
+
+        public static void Run()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var delay = await WorkForARandomTime();
+                    Console.WriteLine($"I waited for {delay} ms");
+                    if (delay > 1900)
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine("done");
+            });
+            Console.ReadLine();
+        }
+
+        public static void RunWithoutAsync()
+        {
+            Task Impl()
+            {
+                return WorkForARandomTime().ContinueWith(t =>
+                {
+                    var delay = t.Result;
+                    Console.WriteLine($"I waited for {delay} ms");
+                    if (delay > 1900)
+                    {
+                        Console.WriteLine("done");
+                        return Task.CompletedTask;
+                    }
+                    else
+                    {
+                        return Impl();
+                    }
+                });
+            }
+
+            Impl();
+            Console.ReadLine();
+        }
+    }
+}
