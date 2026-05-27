a653ebde1a0d78fcacc8acb846ff339e7273b303
Jean-Noël Gourdol <jngourdol@stormancer.com>
2026-05-27 16:15:01 +0200

    small udp sample

---
+++ UdpClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Sockets;
+
+async Task Receive()
+{
+    UdpClient server = null!;
+    try
+    {
+        server = new UdpClient(666);
+        while (true)
+        {
+            var result = await server.ReceiveAsync();
+            Console.WriteLine($"I received a datagram of {result.Buffer.Length} bytes from {result.RemoteEndPoint}");
+            await server.SendAsync(result.Buffer, result.Buffer.Length, result.RemoteEndPoint);
+        }
+    }
+    finally
+    {
+        server?.Dispose();
+    }
+}
+
+_ = Receive();
+
+using var client = new UdpClient();
+await client.SendAsync([1, 2, 3, 4], 4, "localhost", 666);
+var result = await client.ReceiveAsync();
+Console.WriteLine($"I received a datagram of {result.Buffer.Length} bytes from {result.RemoteEndPoint}");
+
+Console.ReadLine();
