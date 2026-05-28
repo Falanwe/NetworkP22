2490d2ceb9ed991fe1ac3d2ac5abba3dffd26512
Jean-Noël Gourdol <jngourdol@stormancer.com>
2026-05-28 10:21:44 +0200

    tcp communication sample

---
+++ TcpClient/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+using (var client = new TcpClient())
+{
+    await client.ConnectAsync("localhost", 667);
+    await client.GetStream().WriteAsync([1, 2, 3, 4], 0, 4);
+    client.Close();
+}
+
+Console.WriteLine("I sent 4 bytes to the server");
+Console.ReadLine();
